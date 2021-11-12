fd6b2da2b126ca2153d8f795cf2b6b3f0636e165
Claudia Hardman <claudia.hardman@mattel.com>
2021-11-11 19:46:25 -0500

    add error reconstruction test

--- http_test.go
+++ http_test.go
@@ -1,9 +1,11 @@
 package errdetails
 
 import (
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 	"google.golang.org/genproto/googleapis/rpc/errdetails"
@@ -60,4 +62,53 @@ func TestHandler(t *testing.T) {
 		}]
 	}`
 	require.JSONEq(t, exp, rr.Body.String())
+}
+
+func TestFromJSON(t *testing.T) {
+	rr := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodOptions, "/", nil)
+
+	handler := HandlerFunc(func(w http.ResponseWriter, r *http.Request) error {
+		return New(codes.ResourceExhausted, "resend email rate limit exceeded",
+			RetryDelay(time.Minute),
+			QuotaFailure(&errdetails.QuotaFailure_Violation{
+				Subject:     "auth0|123456789",
+				Description: "Rate limit applied for Resend Email",
+			}),
+		)
+	})
+	handler.ServeHTTP(rr, req)
+
+	res := rr.Result()
+	defer res.Body.Close()
+
+	err := FromJSON(res.Body)
+
+	if !errors.Is(err, ErrResourceExhausted) {
+		t.Error("expected error to be ErrResourceExhausted")
+	}
+
+	var retErr RetriableError
+	if !errors.As(err, &retErr) {
+		t.Error("expected error to be RetriableError")
+	}
+
+	if got, want := retErr.GetRetryDelay(), time.Minute; got != want {
+		t.Errorf("unexpected recommended retry delay; got %q, want %q", got, want)
+	}
+
+	var qfErr FailedQuotaError
+	if !errors.As(err, &qfErr) {
+		t.Error("expected error to be FailedQuotaError")
+	}
+
+	violation := qfErr.GetViolations()[0]
+	if got, want := violation.GetDescription(), "Rate limit applied for Resend Email"; got != want {
+		t.Errorf("unexpected quota violation description; got %q, want %q", got, want)
+
+	}
+
+	if got, want := violation.GetSubject(), "auth0|123456789"; got != want {
+		t.Errorf("unexpected quota violation subject; got %q, want %q", got, want)
+	}
 }
